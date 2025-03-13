758d97bc25d10b22ea99fb7528d0e7b4f6f26192
qhouyee <quekhouyee@gmail.com>
2025-03-13 17:08:15 +0000

    1466-instantiate-csv-data-as-rdf: removed incompatible rml mapper library

--- stack-clients/src/main/java/com/cmclinnovations/stack/clients/rml/RmlMapperClient.java
+++ stack-clients/src/main/java/com/cmclinnovations/stack/clients/rml/RmlMapperClient.java
@@ -1,10 +1,8 @@
 package com.cmclinnovations.stack.clients.rml;
 
-import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -24,14 +22,6 @@ import com.cmclinnovations.stack.clients.blazegraph.BlazegraphEndpointConfig;
 import com.cmclinnovations.stack.clients.core.ClientWithEndpoint;
 import com.cmclinnovations.stack.clients.core.EndpointNames;
 import com.cmclinnovations.stack.clients.utils.FileUtils;
-
-import be.ugent.rml.Executor;
-import be.ugent.rml.Utils;
-import be.ugent.rml.records.RecordsFactory;
-import be.ugent.rml.store.QuadStore;
-import be.ugent.rml.store.QuadStoreFactory;
-import be.ugent.rml.store.RDF4JStore;
-import be.ugent.rml.term.NamedNode;
 
 public class RmlMapperClient extends ClientWithEndpoint<RmlMapperEndpointConfig> {
   private static final Logger LOGGER = LoggerFactory.getLogger(RmlMapperClient.class);
@@ -76,51 +66,6 @@ public class RmlMapperClient extends ClientWithEndpoint<RmlMapperEndpointConfig>
     BlazegraphEndpointConfig blazegraphConfig = super.readEndpointConfig(EndpointNames.BLAZEGRAPH,
         BlazegraphEndpointConfig.class);
     String sparqlEndpoint = blazegraphConfig.getUrl(namespace);
-    try {
-      // TO DO
-      // TO DO
-      // RML Rules should be parsed and added with sources and targets
-      // Source:
-      // :source_000 a rml:LogicalSource;
-      // rdfs:label "source label";
-      // rml:source "source.csv";
-      // rml:referenceFormulation ql:CSV.
-      // :map_mappingname_000 a rr:TriplesMap; #to id;do not add
-      // rml:logicalSource :source_000.
-      // Target:
-      // :target_000 a rmlt:LogicalTarget;
-      // rdfs:label "sparql";
-      // rmlt:serialization formats:Turtle;
-      // rmlt:target :sd_000.
-      // :sd_000 a sd:Service;
-      // sd:supportedLanguage sd:SPARQL11Update;
-      // sd:endpoint <SPARQL Endpoint>.
-      // :s_000 a rr:SubjectMap; #to id;do not add
-      // rml:logicalTarget :target_000.
-
-      QuadStore rmlStore = QuadStoreFactory.read(rmlRules);
-
-      // To parse the data into records eg each csv row is a record
-      // As we are interested only in local file access, base path is crucial to
-      // determine the absolute path from the root. Mapping path is not required for
-      // this simple case
-      RecordsFactory factory = new RecordsFactory("/", "/");
-
-      // Output store serialises beyond nquads, but is not needed when uploading
-      // directly to Blazegraph
-      QuadStore outputStore = new RDF4JStore();
-
-      // Function agent is not required - the library will default to their base
-      // functions
-      Executor executor = new Executor(rmlStore, factory, outputStore,
-          Utils.getBaseDirectiveTurtleOrDefault(rmlRules, "https://theworldavatar.io/kg/"), null);
-      QuadStore result = executor.execute(null).get(new NamedNode("rmlmapper://default.store"));
-
-      BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-      result.write(out, "turtle");
-    } catch (Exception e) {
-      LOGGER.error(e.getMessage());
-    }
   }
 
   /**
