70aa38a1f4fc8114001c87c13415244afe03f14e
qhouyee <quekhouyee@gmail.com>
2025-03-13 15:36:48 +0000

    1466-instantiate-csv-data-as-rdf: refactor to reuse parent file related methods and use UPPERCASE convention for static const

--- stack-clients/src/main/java/com/cmclinnovations/stack/clients/rml/RmlMapperClient.java
+++ stack-clients/src/main/java/com/cmclinnovations/stack/clients/rml/RmlMapperClient.java
@@ -9,12 +9,13 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Map;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,7 +34,7 @@ import be.ugent.rml.store.RDF4JStore;
 import be.ugent.rml.term.NamedNode;
 
 public class RmlMapperClient extends ClientWithEndpoint<RmlMapperEndpointConfig> {
-  private static final Logger logger = LoggerFactory.getLogger(RmlMapperClient.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(RmlMapperClient.class);
 
   private static final String CSV_FILE_EXTENSION = "csv";
   private static final String YML_FILE_EXTENSION = "yml";
@@ -58,7 +59,7 @@ public class RmlMapperClient extends ClientWithEndpoint<RmlMapperEndpointConfig>
    * @param dirPath Target directory path.
    */
   public ByteArrayOutputStream parseYarrrmlToRml(Path dirPath) {
-    logger.info("Checking and parsing YARRRML files...");
+    LOGGER.info("Checking and parsing YARRRML files...");
     this.validateDirContents(dirPath);
     return this.genRmlRules(dirPath);
   }
@@ -71,7 +72,7 @@ public class RmlMapperClient extends ClientWithEndpoint<RmlMapperEndpointConfig>
    * @param namespace Target namespace to upload the converted RDF triples.
    */
   public void parseRmlToRDF(InputStream rmlRules, String namespace) {
-    logger.info("Reading the RML rules...");
+    LOGGER.info("Reading the RML rules...");
     BlazegraphEndpointConfig blazegraphConfig = super.readEndpointConfig(EndpointNames.BLAZEGRAPH,
         BlazegraphEndpointConfig.class);
     String sparqlEndpoint = blazegraphConfig.getUrl(namespace);
@@ -118,7 +119,7 @@ public class RmlMapperClient extends ClientWithEndpoint<RmlMapperEndpointConfig>
       BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
       result.write(out, "turtle");
     } catch (Exception e) {
-      logger.error(e.getMessage());
+      LOGGER.error(e.getMessage());
     }
   }
 
@@ -132,7 +133,7 @@ public class RmlMapperClient extends ClientWithEndpoint<RmlMapperEndpointConfig>
     Collection<URI> csvFiles = this.getFiles(dirPath, CSV_FILE_EXTENSION);
     Collection<URI> ymlFiles = this.getFiles(dirPath, YML_FILE_EXTENSION);
     if (csvFiles.size() != ymlFiles.size()) {
-      logger.error("Detected missing file pairs with {} csv and {} yml files! Ensure files are even.",
+      LOGGER.error("Detected missing file pairs with {} csv and {} yml files! Ensure files are even.",
           csvFiles.size(), ymlFiles.size());
       throw new IllegalArgumentException();
     }
@@ -144,27 +145,31 @@ public class RmlMapperClient extends ClientWithEndpoint<RmlMapperEndpointConfig>
    * @param dirPath Target directory path.
    */
   private ByteArrayOutputStream genRmlRules(Path dirPath) {
-    logger.info("Converting the YARRRML inputs into RML rules...");
+    LOGGER.info("Converting the YARRRML inputs into RML rules...");
     String containerId = super.getContainerId(super.getContainerName());
     Collection<URI> ymlFiles = this.getFiles(dirPath, YML_FILE_EXTENSION);
-    StringBuilder inputCommand = new StringBuilder();
-    // Iterate to copy each file and add the copied file path to the CMD command
-    for (URI file : ymlFiles) {
-      String fileName = Paths.get(file).getFileName().toString();
-      String targetFilePath = "/data/" + fileName;
-      this.sendFile(containerId, Paths.get(targetFilePath), this.getFileContents(file));
-      // Add white space after the previous commands
-      inputCommand.append(" -i ").append(targetFilePath);
-    }
+    List<String> commandList = new ArrayList<>(List.of(YARRRML_PARSER_EXECUTABLE_PATH));
+
+    // Convert from URI to String and append them to command
+    List<String> filePaths = ymlFiles.stream()
+        .map(ymlFile -> {
+          String fileName = Paths.get(ymlFile).getFileName().toString();
+          commandList.add("-i");
+          commandList.add("/data/" + fileName);
+          return fileName;
+        })
+        .collect(Collectors.toList());
+    // Send the files to a new data directory
+    super.sendFiles(containerId, dirPath.toAbsolutePath().toString(), filePaths, "/data");
 
     // Execute the command and return the RML mappings as streams
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     ByteArrayOutputStream errorStream = new ByteArrayOutputStream();
-    String execId = super.createComplexCommand(containerId, YARRRML_PARSER_EXECUTABLE_PATH, inputCommand.toString())
+    String execId = super.createComplexCommand(containerId, commandList.toArray(new String[0]))
         .withOutputStream(outputStream)
         .withErrorStream(errorStream)
         .exec();
-    super.handleErrors(errorStream, execId, logger);
+    super.handleErrors(errorStream, execId, LOGGER);
     return outputStream;
   }
 
@@ -176,43 +181,17 @@ public class RmlMapperClient extends ClientWithEndpoint<RmlMapperEndpointConfig>
    */
   private Collection<URI> getFiles(Path dirPath, String fileExtension) {
     try {
-      logger.info(MessageFormat.format("Getting all {0} files from directory {1} ...", fileExtension,
+      LOGGER.info(MessageFormat.format("Getting all {0} files from directory {1} ...", fileExtension,
           dirPath.toAbsolutePath()));
       URL dirUrl = dirPath.toUri().toURL();
       return FileUtils.listFiles(dirUrl, fileExtension);
     } catch (URISyntaxException | MalformedURLException ex) {
-      logger.error(ex.getMessage());
-      throw new IllegalArgumentException("Invalid directory path: " + ex.getMessage());
+      LOGGER.error("Invalid directory path: {} . Read error message for more information: {}", dirPath,
+          ex.getMessage());
+      throw new IllegalArgumentException(MessageFormat.format("Invalid directory path: {0}", dirPath));
     } catch (IOException ex) {
-      logger.error(ex.getMessage());
-      throw new RuntimeException("Failed to list files in the directory: " + ex.getMessage());
+      LOGGER.error(ex.getMessage());
+      throw new RuntimeException(MessageFormat.format("Failed to list files in the directory: {0}", ex.getMessage()));
     }
-  }
-
-  /**
-   * Retrieves the contents of the specified file.
-   * 
-   * @param file Target file.
-   */
-  private byte[] getFileContents(URI file) {
-    try {
-      Path path = Paths.get(file);
-      return Files.readAllBytes(path);
-    } catch (IOException ex) {
-      logger.error(ex.getMessage());
-      throw new RuntimeException("Unable to read file: " + ex.getMessage());
-    }
-  }
-
-  /**
-   * Copy the file contents into the specified container at the target path.
-   * 
-   * @param containerId ID of the target container.
-   * @param filePath    Target file path location in the container.
-   * @param content     Contents to be copied.
-   */
-  private void sendFile(String containerId, Path filePath, byte[] content) {
-    sendFilesContent(containerId, Map.of(filePath.getFileName().toString(), content),
-        filePath.getParent().toString());
   }
 }
