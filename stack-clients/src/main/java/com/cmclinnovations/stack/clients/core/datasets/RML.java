c8379ca1b47536ffbc800aa0ccdd1b8878895404
qhouyee <quekhouyee@gmail.com>
2025-03-13 15:34:54 +0000

    1466-instantiate-csv-data-as-rdf: rename RMLDataSubset.java to RML.java

--- stack-clients/src/main/java/com/cmclinnovations/stack/clients/core/datasets/RML.java
+++ stack-clients/src/main/java/com/cmclinnovations/stack/clients/core/datasets/RML.java
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 
 import com.cmclinnovations.stack.clients.rml.RmlMapperClient;
 
-public class RmlDataSubset extends DataSubset {
+public class RML extends DataSubset {
 
   @Override
   public boolean usesBlazegraph() {
