85ab018f53b17dfce606f77de284ac54bd87e1dc
qhouyee <quekhouyee@gmail.com>
2025-03-20 13:17:09 +0000

    1466-instantiate-csv-data-as-rdf: refactor to read files for testing of expected output, and moved current rules into a nested folder

--- stack-clients/src/test/java/com/cmclinnovations/stack/clients/utils/YarrrmlFileTest.java
+++ stack-clients/src/test/java/com/cmclinnovations/stack/clients/utils/YarrrmlFileTest.java
@@ -2,33 +2,43 @@ package com.cmclinnovations.stack.clients.utils;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.yaml.snakeyaml.Yaml;
 
 class YarrrmlFileTest {
         private static final String TEST_ENDPOINT = "https://example.org/kg/sparql";
-        private static final String TEST_ONE_FILE_NAME = "rules.yml";
-        private static final String TEST_TWO_FILE_NAME = "rules2.yml";
-        private static final String TEST_THREE_FILE_NAME = "rules_functions.yml";
-        private static final String TEST_FOUR_FILE_NAME = "rules2_functions.yml";
+        private static final String TEST_ONE_FILE_NAME = "yml/test/rules.yml";
+        private static final String EXPECTED_ONE_FILE_NAME = "yml/expected/rules.yml";
+        private static final String TEST_TWO_FILE_NAME = "yml/test/rules2.yml";
+        private static final String EXPECTED_TWO_FILE_NAME = "yml/expected/rules2.yml";
+        private static final String TEST_THREE_FILE_NAME = "yml/test/rules_functions.yml";
+        private static final String EXPECTED_THREE_FILE_NAME = "yml/expected/rules_functions.yml";
+        private static final String TEST_FOUR_FILE_NAME = "yml/test/rules2_functions.yml";
+        private static final String EXPECTED_FOUR_FILE_NAME = "yml/expected/rules2_functions.yml";
 
         @Test
         void testDefaultConstructor() {
                 YarrrmlFile yarrrmlFile = new YarrrmlFile();
-                 Assertions.assertEquals(new HashMap<>(), yarrrmlFile.getRules());
+                Assertions.assertEquals(new HashMap<>(), yarrrmlFile.getRules());
         }
 
         @Test
         void testFileConstructor() throws IOException, URISyntaxException {
                 Path rulesFilePath = Paths.get(YarrrmlFileTest.class.getResource(TEST_ONE_FILE_NAME).toURI());
                 YarrrmlFile yarrrmlFile = new YarrrmlFile(rulesFilePath, TEST_ENDPOINT);
-                 Assertions.assertEquals(this.genExpectedYarrrmlContents(rulesFilePath.toString()), yarrrmlFile.getRules());
+                Assertions.assertEquals(
+                                this.genExpectedYarrrmlContents(
+                                                YarrrmlFileTest.class.getResource(EXPECTED_ONE_FILE_NAME),
+                                                rulesFilePath, TEST_ENDPOINT),
+                                yarrrmlFile.getRules());
         }
 
         @Test
@@ -36,7 +46,10 @@ class YarrrmlFileTest {
                 Path rulesFilePath = Paths.get(YarrrmlFileTest.class.getResource(TEST_ONE_FILE_NAME).toURI());
                 YarrrmlFile yarrrmlFile = new YarrrmlFile();
                 yarrrmlFile.addRules(rulesFilePath, TEST_ENDPOINT);
-                 Assertions.assertEquals(this.genExpectedYarrrmlContents(rulesFilePath.toString()), yarrrmlFile.getRules());
+                Assertions.assertEquals(this.genExpectedYarrrmlContents(
+                                YarrrmlFileTest.class.getResource(EXPECTED_ONE_FILE_NAME),
+                                rulesFilePath, TEST_ENDPOINT),
+                                yarrrmlFile.getRules());
         }
 
         @Test
@@ -44,7 +57,10 @@ class YarrrmlFileTest {
                 Path rulesFilePath = Paths.get(YarrrmlFileTest.class.getResource(TEST_TWO_FILE_NAME).toURI());
                 YarrrmlFile yarrrmlFile = new YarrrmlFile();
                 yarrrmlFile.addRules(rulesFilePath, TEST_ENDPOINT);
-                 Assertions.assertEquals(this.genExpectedYarrrmlContents(rulesFilePath.toString()), yarrrmlFile.getRules());
+                Assertions.assertEquals(this.genExpectedYarrrmlContents(
+                                YarrrmlFileTest.class.getResource(EXPECTED_TWO_FILE_NAME),
+                                rulesFilePath, TEST_ENDPOINT),
+                                yarrrmlFile.getRules());
         }
 
         @Test
@@ -52,7 +68,9 @@ class YarrrmlFileTest {
                 Path rulesFilePath = Paths.get(YarrrmlFileTest.class.getResource(TEST_THREE_FILE_NAME).toURI());
                 YarrrmlFile yarrrmlFile = new YarrrmlFile();
                 yarrrmlFile.addRules(rulesFilePath, TEST_ENDPOINT);
-                 Assertions.assertEquals(this.genExpectedYarrrmlFunctionContents(rulesFilePath.toString()),
+                Assertions.assertEquals(this.genExpectedYarrrmlContents(
+                                YarrrmlFileTest.class.getResource(EXPECTED_THREE_FILE_NAME),
+                                rulesFilePath, TEST_ENDPOINT),
                                 yarrrmlFile.getRules());
         }
 
@@ -61,94 +79,26 @@ class YarrrmlFileTest {
                 Path rulesFilePath = Paths.get(YarrrmlFileTest.class.getResource(TEST_FOUR_FILE_NAME).toURI());
                 YarrrmlFile yarrrmlFile = new YarrrmlFile();
                 yarrrmlFile.addRules(rulesFilePath, TEST_ENDPOINT);
-                 Assertions.assertEquals(this.genExpectedYarrrmlFunctionContents(rulesFilePath.toString()),
+                Assertions.assertEquals(this.genExpectedYarrrmlContents(
+                                YarrrmlFileTest.class.getResource(EXPECTED_FOUR_FILE_NAME),
+                                rulesFilePath, TEST_ENDPOINT),
                                 yarrrmlFile.getRules());
         }
 
-        private Map<String, Object> genExpectedYarrrmlContents(String fileName) {
-                Map<String, Object> yamlData = this.genExpectedYarrrmlTemplate(fileName);
+        private AliasMap<Object> genExpectedYarrrmlContents(URL expectedFilePath, Path expectedSourceLocation,
+                        String endpoint)
+                        throws IOException, URISyntaxException {
+                Yaml yaml = new Yaml();
+                String content = new String(Files.readAllBytes(Paths.get(expectedFilePath.toURI())));
+                String modifiedContent = content
+                                .replace("[source]",
+                                                FileUtils.replaceExtension(expectedSourceLocation.toString(), "csv"))
+                                .replace("[target]", endpoint);
 
-                Map<String, Map<String, Object>> mappings = new HashMap<>();
+                Map<String, Object> loadedMap = yaml.load(modifiedContent);
 
-                mappings.put("person", Map.of(
-                                "sources", "source-ref",
-                                "s", Map.of(
-                                                "value", "base:person/$(id)",
-                                                "targets", "target-ref"),
-                                "po", List.of(this.genExpectedYarrrmlPredObj("a", "base:Person"),
-                                                this.genExpectedYarrrmlPredObj("base:hasName",
-                                                                "base:person/name/$(id)~iri"))));
-
-                mappings.put("person-name", Map.of(
-                                "sources", "source-ref",
-                                "s", Map.of(
-                                                "value", "base:person/name/$(id)",
-                                                "targets", "target-ref"),
-                                "po", List.of(this.genExpectedYarrrmlPredObj("a", "base:PersonName"),
-                                                this.genExpectedYarrrmlPredObj("rdfs:label", "$(name)"))));
-
-                yamlData.put("mappings", mappings);
-                return yamlData;
-        }
-
-        private Map<String, Object> genExpectedYarrrmlFunctionContents(String fileName) {
-                Map<String, Object> yamlData = this.genExpectedYarrrmlTemplate(fileName);
-                Map<String, Map<String, Object>> mappings = new HashMap<>();
-
-                mappings.put("person", Map.of(
-                                "sources", "source-ref",
-                                "s", this.genExpectedYarrrmlFunction(
-                                                "grel:string_trim",
-                                                "base:person/$(id)~iri",
-                                                Map.of("targets", "target-ref")),
-                                "po", List.of(this.genExpectedYarrrmlPredObj("a", "base:Person"),
-                                                this.genExpectedYarrrmlPredObj("base:hasName",
-                                                                this.genExpectedYarrrmlFunction(
-                                                                                "grel:toLowerCase",
-                                                                                this.genExpectedYarrrmlFunction(
-                                                                                                "grel:string_trim",
-                                                                                                "base:department/name/$(name)~iri"))))));
-                yamlData.put("mappings", mappings);
-                return yamlData;
-        }
-
-        private Map<String, Object> genExpectedYarrrmlTemplate(String fileName) {
-                Map<String, Object> yamlData = new AliasMap<>();
-                yamlData.put("prefixes", Map.of(
-                                "rdfs", "http://www.w3.org/2000/01/rdf-schema#",
-                                "base", "https://theworldavatar.io/kg/",
-                                "grel", "http://users.ugent.be/~bjdmeest/function/grel.ttl#"));
-                yamlData.put("sources", Map.of(
-                                "source-ref", Map.of(
-                                                "referenceFormulation", "csv",
-                                                "access", FileUtils.replaceExtension(fileName, "csv"))));
-                yamlData.put("targets", Map.of(
-                                "target-ref", Map.of(
-                                                "serialization", "turtle",
-                                                "access", TEST_ENDPOINT,
-                                                "type", "sd")));
-                return yamlData;
-        }
-
-        private Map<String, Object> genExpectedYarrrmlPredObj(String predVal, Object objVal) {
-                return Map.of("p", predVal,
-                                "o", objVal);
-        }
-
-        private Map<String, Object> genExpectedYarrrmlFunction(String function, Object paramVal) {
-                return this.genExpectedYarrrmlFunction(function, paramVal, null);
-        }
-
-        private Map<String, Object> genExpectedYarrrmlFunction(String function, Object paramVal,
-                        Map<String, Object> additionalFields) {
-                Map<String, Object> result = new HashMap<>();
-                result.put("function", function);
-                result.put("parameters", List.of(
-                                Map.of("parameter", "grel:valueParameter",
-                                                "value", paramVal)));
-                if (additionalFields != null && !additionalFields.isEmpty()) {
-                        result.putAll(additionalFields);
-                }
-                return result;
+                AliasMap<Object> rules = new AliasMap<>();
+                rules.putAll(loadedMap);
+                return rules;
         }
 }
