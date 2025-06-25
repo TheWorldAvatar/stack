41e12bad1853f8bf33b1c2269bbd4edf32942983
George Brownbridge <gbrownbridge@cmcl.io>
2025-06-25 16:18:44 +0100

    35-skip-image-pulling: Added a comment about the data-uploader never pulling images.

--- stack-data-uploader/src/main/java/com/cmclinnovations/stack/DataUploader.java
+++ stack-data-uploader/src/main/java/com/cmclinnovations/stack/DataUploader.java
@@ -9,6 +9,7 @@ import com.cmclinnovations.stack.clients.core.datasets.DatasetLoader;
  */
 public class DataUploader {
     public static void main(String[] args) {
+        // The data uploader should *never* try to pull images!
         StackClient.setIsolated(true);
 
         new DatasetLoader().loadInputDatasets(StackClient.STACK_CONFIG_DIR, StackClient.getStackName());
