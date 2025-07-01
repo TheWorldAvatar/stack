10383b28bf28d61b42dcbe8cd04d956c3cd1b8f2
George Brownbridge <gbrownbridge@cmcl.io>
2025-07-01 17:47:07 +0100

    rest-api-agent: Added a way to build (partial) URLs from the `StackHost` data, with the option of including default values for each component. Also added some tests for the `StackHost`  class.

--- stack-clients/src/main/java/com/cmclinnovations/stack/clients/core/StackHost.java
+++ stack-clients/src/main/java/com/cmclinnovations/stack/clients/core/StackHost.java
@@ -1,8 +1,8 @@
 package com.cmclinnovations.stack.clients.core;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Optional;
-
-import com.cmclinnovations.stack.clients.utils.FileUtils;
 
 public final class StackHost {
 
@@ -36,6 +36,110 @@ public final class StackHost {
 
     public Optional<String> getPath() {
         return path;
+    }
+
+    public Builder getStringBuilder() {
+        return new Builder();
+    }
+
+    public class Builder {
+
+        private String protoOut = null;
+        private String nameOut = null;
+        private String portOut = null;
+        private String pathOut = null;
+        private String extraPathOut = null;
+        private boolean finalSlash = false;
+
+        public Builder withProto() {
+            proto.ifPresent(p -> protoOut = p);
+            return this;
+        }
+
+        public Builder withProto(String defaultProto) {
+            protoOut = proto.orElse(defaultProto);
+            return this;
+        }
+
+        public Builder withName() {
+            name.ifPresent(n -> nameOut = n);
+            return this;
+        }
+
+        public Builder withName(String defaultName) {
+            nameOut = name.orElse(defaultName);
+            return this;
+        }
+
+        public Builder withPort() {
+            port.ifPresent(p -> portOut = p);
+            return this;
+        }
+
+        public Builder withPort(String defaultPort) {
+            portOut = port.orElse(defaultPort);
+            return this;
+        }
+
+        public Builder withPath() {
+            path.ifPresent(p -> pathOut = p);
+            return this;
+        }
+
+        public Builder withPath(String defaultPath) {
+            pathOut = path.orElse(defaultPath);
+            return this;
+        }
+
+        public Builder withExtraPath(String extraPath) {
+            extraPathOut = extraPath;
+            return this;
+        }
+
+        public Builder withFinalSlash() {
+            finalSlash = true;
+            return this;
+        }
+
+        public String build() {
+            return toString();
+        }
+
+        public String toString() {
+            try {
+                // Build the path
+                String fullPath = null;
+                if (null != pathOut) {
+                    fullPath = pathOut;
+                    if (null != extraPathOut)
+                        fullPath += "/" + extraPathOut;
+                } else if (null != extraPathOut) {
+                    fullPath = extraPathOut;
+                }
+                if (null != fullPath) {
+                    // Append leading slash
+                    fullPath = "/" + fullPath;
+                    // Remove duplicate slashes
+                    fullPath = fullPath.replaceAll("/+", "/");
+                }
+
+                // Create formal URI
+                URI uri = (null != portOut)
+                        ? new URI(protoOut, null, nameOut, Integer.parseInt(portOut), fullPath, null, null)
+                        : new URI(protoOut, nameOut, fullPath, null);
+
+                // Remove leading double slashes if no protocol is specified
+                String cleanedString = uri.toString().replaceFirst("^//", "");
+                // Append final slash if requested
+                return finalSlash
+                        ? cleanedString.replaceFirst("([^/])$", "$1/")
+                        : cleanedString.replaceFirst("/$", "");
+
+            } catch (URISyntaxException e) {
+                throw new RuntimeException("Invalid URI syntax: " + e.getMessage(), e);
+            }
+        }
+
     }
 
 }
