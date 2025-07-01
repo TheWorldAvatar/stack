4ec3d49ff88681badd31c5761609035f516c2f10
George Brownbridge <gbrownbridge@cmcl.io>
2025-07-01 17:48:39 +0100

    rest-api-agent: Added code to set the `GRLC_SERVER_NAME` environment variable for the grlc container if the `StackHost` is specified.

--- stack-clients/src/main/java/com/cmclinnovations/stack/services/GrlcService.java
+++ stack-clients/src/main/java/com/cmclinnovations/stack/services/GrlcService.java
@@ -1,6 +1,7 @@
 package com.cmclinnovations.stack.services;
 
 import com.cmclinnovations.stack.clients.core.EndpointNames;
+import com.cmclinnovations.stack.clients.core.StackClient;
 import com.cmclinnovations.stack.clients.rdf4j.Rdf4jEndpointConfig;
 import com.cmclinnovations.stack.services.config.ServiceConfig;
 
@@ -8,6 +9,7 @@ public class GrlcService extends ContainerService {
 
     public static final String TYPE = "grlc";
 
+    private static final String GRLC_SERVER_NAME_KEY = "GRLC_SERVER_NAME";
     private static final String GRLC_SPARQL_ENDPOINT_KEY = "GRLC_SPARQL_ENDPOINT";
 
     public GrlcService(String stackName, ServiceConfig config) {
@@ -17,6 +19,19 @@ public class GrlcService extends ContainerService {
     @Override
     protected void doPreStartUpConfiguration() {
 
+        // String serverName = (StackClient.getStackHost()
+        // .getWithDefaults(null, "", "", "") + "rest/")
+        // .replaceAll("/+", "\\\\/");
+
+        String serverName = StackClient.getStackHost().getStringBuilder()
+                .withName()
+                .withPort()
+                .withPath()
+                .withExtraPath("rest")
+                .build()
+                .replaceAll("/+", "\\\\/");
+
+        setEnvironmentVariableIfAbsent(GRLC_SERVER_NAME_KEY, serverName);
         setEnvironmentVariableIfAbsent(GRLC_SPARQL_ENDPOINT_KEY,
                 readEndpointConfig(EndpointNames.RDF4J, Rdf4jEndpointConfig.class)
                         .getOutgoingRepositoryUrl().toString());
